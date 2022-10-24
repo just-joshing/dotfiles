ed7e6d25085457c64e668cd000247d586e055b00
Joshua French <jgg_63@yahoo.com>
2022-10-24 12:18:59 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Get Homebrew
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
 # Clone P10K prompt
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
 
